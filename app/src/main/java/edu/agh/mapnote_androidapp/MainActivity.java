f77400aab133fba10e271b259871cf36a082c1cb
jwojnarowicz <yakub.wojnarowicz@gmail.com>
2022-03-23 22:04:14 +0100

    UI prototype + Gradle dependency

--- app/src/main/java/edu/agh/mapnote_androidapp/MainActivity.java
+++ app/src/main/java/edu/agh/mapnote_androidapp/MainActivity.java
@@ -3,12 +3,31 @@ package edu.agh.mapnote_androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_latitude, tv_longitude, tv_address, tv_updates, tv_accuracy;
+
+    Switch sw_updates, sw_accuracy;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_latitude = findViewById(R.id.tv_latitude);
+        tv_longitude = findViewById(R.id.tv_longitude);
+        tv_address = findViewById(R.id.tv_address);
+        tv_updates = findViewById(R.id.tv_localisationStatus);
+        tv_accuracy = findViewById(R.id.tv_accuracyStatus);
+        sw_updates = findViewById(R.id.sw_updates);
+        sw_accuracy = findViewById(R.id.sw_accuracy);
+
+
     }
 }
