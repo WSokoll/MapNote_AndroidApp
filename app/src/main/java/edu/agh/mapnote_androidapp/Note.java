0e79df4268b23f5166561c886bb3b5c3d4b5e1f3
WSokoll <wladek.sokol01@gmail.com>
2022-03-23 22:35:26 +0100

    add Note class

---
+++ app/src/main/java/edu/agh/mapnote_androidapp/Note.java
@@ -0,0 +1,80 @@
+package edu.agh.mapnote_androidapp;
+
+public class Note {
+
+    private int id;
+    private double latitude;
+    private double longitude;
+    private String address;
+    private String date;
+    private String noteContent;
+
+    public Note(int id, double latitude, double longitude, String address, String date, String noteContent) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+        this.date = date;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", address='" + address + '\'' +
+                ", date='" + date + '\'' +
+                ", noteContent='" + noteContent + '\'' +
+                '}';
+    }
+}
