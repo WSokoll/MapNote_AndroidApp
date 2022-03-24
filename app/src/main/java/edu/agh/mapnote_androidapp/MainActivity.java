81b9c2a5d1b5656e7f0959f591c5dd4a7f19310c
WSokoll <wladek.sokol01@gmail.com>
2022-03-24 12:14:56 +0100

    update

--- app/src/main/java/edu/agh/mapnote_androidapp/MainActivity.java
+++ app/src/main/java/edu/agh/mapnote_androidapp/MainActivity.java
@@ -11,18 +11,22 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
-//elements from GUI
+
+    //elements from GUI
     TextView tv_latitude, tv_longitude, tv_address, tv_updates, tv_accuracy;
     Switch sw_updates, sw_accuracy;
-//client Google API
+
+    //client Google API
     private FusedLocationProviderClient fusedLocationClient;
-//location request
-    LocationRequest locationRequest;
+
+    //location request
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tv_latitude = findViewById(R.id.tv_latitude);
         tv_longitude = findViewById(R.id.tv_longitude);
         tv_address = findViewById(R.id.tv_address);
@@ -30,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
         tv_accuracy = findViewById(R.id.tv_accuracyStatus);
         sw_updates = findViewById(R.id.sw_updates);
         sw_accuracy = findViewById(R.id.sw_accuracy);
-//zalążek clienta lokalizacji
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        //zalążek clienta lokalizacji
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
     }
 }
