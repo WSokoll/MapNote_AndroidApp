bd0eca54d56a02a19e7b3ec0ac661fec007abeff
WSokoll <wladek.sokol01@gmail.com>
2022-03-26 20:31:39 +0100

    add ViewNotesActivity

---
+++ app/src/main/java/edu/agh/mapnote_androidapp/ViewNotesActivity.java
@@ -0,0 +1,14 @@
+package edu.agh.mapnote_androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_notes);
+    }
+}
