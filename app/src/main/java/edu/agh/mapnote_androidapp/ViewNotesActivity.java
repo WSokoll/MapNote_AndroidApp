4cbc00119a069faa8e1376183be1b0e75b42544a
WSokoll <wladek.sokol01@gmail.com>
2022-03-27 18:14:42 +0200

    update

--- app/src/main/java/edu/agh/mapnote_androidapp/ViewNotesActivity.java
+++ app/src/main/java/edu/agh/mapnote_androidapp/ViewNotesActivity.java
@@ -7,16 +7,31 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
 public class ViewNotesActivity extends AppCompatActivity {
 
     ListView lv_Notes;
+
+    private DbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_notes);
 
-        String[] values = new String[]{"abc", "123", "456"};
+        dbHelper = new DbHelper(this);
+
+        //get list of notes from the database
+        List<Note> noteList = dbHelper.getAllNotes();
+
+        //create String table with notes
+        String[] values = new String[noteList.size()];
+        int i = 0;
+        for(Note note : noteList){
+            values[i] = note.toString();
+            i += 1;
+        }
 
         lv_Notes = findViewById(R.id.lv_Notes);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
