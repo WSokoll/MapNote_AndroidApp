bf326c01bbfb67b2732d75271e73649621cb76d6
WSokoll <wladek.sokol01@gmail.com>
2022-03-29 20:47:16 +0200

    add EditDeleteNoteActivity

---
+++ app/src/main/java/edu/agh/mapnote_androidapp/EditDeleteNoteActivity.java
@@ -0,0 +1,14 @@
+package edu.agh.mapnote_androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditDeleteNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_delete_note);
+    }
+}
